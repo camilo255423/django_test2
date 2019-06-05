de8eabd029bde145fd3442158198e8ba4b594656
camilo <rodriguez.camilo@gmail.com>
2019-06-05 18:22:05 -0500

    tutorial02

--- site1/app1/views.py
+++ site1/app1/views.py
@@ -1,33 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from .forms import NameForm
 # Create your views here.
 
 
-def hello(request):
-    print(request.POST)
-    datos = [1, 2, 3]
-    nombre = request.GET['nombre']
-
-    return render(request, "hello.html", {'datos': datos, 'nombre': nombre})
-
-
-def get_name(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/hello/')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NameForm()
-
-    return render(request, 'forms.html', {'form': form})
+def index(request):
+    return HttpResponse("hello")
--- site1/site1/urls.py
+++ site1/site1/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-import app1.views
+from django.urls import include, path
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', app1.views.hello),
-    path('name/', app1.views.get_name),
-]
+    path('polls/', include('app1.urls'))
+    ]
